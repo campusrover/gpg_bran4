1757fd6623107dc0a20ee70b0c9ba80fd48bd035
Pito Salas <pitosalas@users.noreply.github.com>
2024-03-04 16:26:12 -0500

    update

--- linorobot/teensy/firmware/lib/branled/branled.cpp
+++ linorobot/teensy/firmware/lib/branled/branled.cpp
@@ -1,32 +1,35 @@
 #include "branled.h"
 #include "campusrover.h"
-#include <Wire.h>
 #include <Adafruit_MCP23X17.h>
- 
-#define LEFT_LED_PIN 1      // MCP23XXX pin Left LED is attached to
-#define RIGHT_LED_PIN 2     // MCP23XXX pin Right LED is attached to
+#include <Wire.h>
+
+#define LEFT_LED_PIN 1  // MCP23XXX pin Left LED is attached to
+#define RIGHT_LED_PIN 2 // MCP23XXX pin Right LED is attached to
 
 Adafruit_MCP23X17 mcp;
 
-BrandeisLED::BrandeisLED() : node_handle(nullptr) {
-};
+BrandeisLED::BrandeisLED() : node_handle(nullptr){};
 
-void BrandeisLED::setup(ros::NodeHandle &nh) { 
-    node_handle = &nh;
-    Serial.begin(9600);
-    i2c_ok = true;
+void BrandeisLED::setup(ros::NodeHandle &nh) {
+  node_handle = &nh;
+  i2c_ok = true;
 
-    if (!mcp.begin_I2C(0x58)) {
-        i2c_ok = false;
-    }
-      // configure LED pin for output
-    mcp.pinMode(RIGHT_LED_PIN, OUTPUT);
-    mcp.pinMode(LEFT_LED_PIN, OUTPUT);
- 
+  if (!mcp.begin_I2C(0x58)) {
+    i2c_ok = false;
+  }
+  // configure LED pin for output
+  mcp.pinMode(RIGHT_LED_PIN, OUTPUT);
+  mcp.pinMode(LEFT_LED_PIN, OUTPUT);
+}
 
-
-  servo_driver = Adafruit_PWMServoDriver(0x40);
-  iteration_time = millis();
-  iteration_interval = 20; // 20 milli second
-  state = "idle";
-
+void BrandeisLed::disconnectedLoop() {
+  if (!i2c_ok) {
+    return;
+  }
+  mcp.digitalWrite(LEFT_LED_PIN, HIGH);
+  mcp.digitalWrite(RIGHT_LED_PIN, LOW);
+  delay(25);
+  mcp.digitalWrite(LEFT_LED_PIN, LOW);
+  mcp.digitalWrite(RIGHT_LED_PIN, HIGH);
+  delay(25);
+}
